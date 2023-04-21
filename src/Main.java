ecce15beca2ddf8e0a03b4d841ec569aabe14d61
thang-39 <91838559+thang-39@users.noreply.github.com>
2023-04-21 22:33:29 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Diego", "Andressa", "Gandalf", "Melina");
+        Map<String, String> nameMap = names
+                .stream()
+                .map (String::toLowerCase)
+                .collect(Collectors.toMap (String::new, Function.identity()));
+        System.out.println(nameMap);
+
+        List<String> listWithDuplicates = Arrays.asList("Gremio", "Gremio", "Gremio", "Barcelona", "Gremio");
+        Set<String> result = listWithDuplicates
+                .stream()
+                .filter( e -> !e.startsWith ("В") )
+                .collect (Collectors.toSet());
+        System.out.println("Removed Duplicates: " + result);
+
+        List<String> cities = Arrays.asList("Porto", "Cabo", "Rio", "Planalto", "Cachorro");
+        String joined = cities
+                .stream()
+                .collect(Collectors. joining ( " velho, "));
+        System.out.println("Joined: " + joined);
+    }
+}
